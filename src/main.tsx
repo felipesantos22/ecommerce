b105b21787adf94028d1bf701142a822c5e789f5
felipesantos22 <felipevs2018@gmail.com>
2023-05-27 12:36:57 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home.tsx';
+import Contact from './pages/Contact.tsx';
+import About from './pages/About.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
